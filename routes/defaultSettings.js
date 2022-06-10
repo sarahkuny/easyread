eb5d6a725cd8ef1b8344e3cb87b03f449d269023
Sarah Kuny <sarahkuny@icloud.com>
2022-06-09 23:49:54 -0500

    scaffold defaultSettings route

--- routes/defaultSettings.js
+++ routes/defaultSettings.js
@@ -1,3 +1,32 @@
 const express = require("express");
 const router = express.Router();
 const db = require("../model/helper");
+const userShouldBeLoggedIn = require("../guards/userShouldBeLoggedIn")
+
+
+//GET settings by user_id 
+router.get('/:id', function(req, res, next) {
+  
+});
+
+//POST settings 
+//(require user_id (foreign key from users table),font_size, font_color, background_color, line_spacing)
+router.post('/', function(req, res, next) {
+
+})
+
+//PUT: edit settings
+router.put('/:id', function(req, res, next){
+
+})
+
+//DELETE settings (set back to app default)
+router.delete('/:id', function(req,res, next) {
+  
+})
+
+
+
+
+
+module.exports = router;
