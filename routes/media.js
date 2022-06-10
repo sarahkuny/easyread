f39a52c9f786d3f44c5a1f29eb3f2ffebfad12cf
Sarah Kuny <sarahkuny@icloud.com>
2022-06-10 10:51:37 -0500

    create get skeleton

--- routes/media.js
+++ routes/media.js
@@ -2,3 +2,8 @@ const express = require("express");
 const router = express.Router();
 const db = require("../model/helper");
 const userShouldBeLoggedIn = require("../guards/userShouldBeLoggedIn")
+
+//GET media
+router.get('/', function(req, res, next) {
+
+})
