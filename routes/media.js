4900dc40048f254e8a29d61ec7773b7ecaace87d
Sarah Kuny <sarahkuny@sarahs-air.lan>
2022-06-10 18:33:19 -0500

    modify get by id endpoint

--- routes/media.js
+++ routes/media.js
@@ -4,12 +4,25 @@ const db = require("../model/helper");
 const userShouldBeLoggedIn = require("../guards/userShouldBeLoggedIn")
 
 //GET media
-router.get('/', function(req, res, next) {
+router.get('/', userShouldBeLoggedIn, function(req, res, next) {
+
+})
+
+//GET by id
+router.get('/:id', userShouldBeLoggedIn, function(req, res, next){
 
 })
 
 //POST media
+router.post('/', userShouldBeLoggedIn, function(req, res, next){
+
+})
 
 //PUT media
+router.put('/', userShouldBeLoggedIn, function(req, res, next){
 
+})
 //DELETE media
+router.delete('/', userShouldBeLoggedIn, function(req, res, next){
+
+})
