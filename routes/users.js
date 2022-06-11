b48086f3cad841763e289f12ddbb979103fd8e2d
Sarah Kuny <sarahkuny@sarahs-air.lan>
2022-06-11 10:57:10 -0500

    add mockaroo data for users table

--- routes/users.js
+++ routes/users.js
@@ -10,7 +10,7 @@ router.use(express.json())
 //GET all users
 //helpful for development, but comment out before deploying
 router.get('/', function(req, res, next) {
-  
+
 });
 
 //POST user (require first_name, last_name, username, password --> store password as hashed password using bcrypt)
