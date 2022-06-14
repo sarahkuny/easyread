faa9219455898136bb6c212f6eb33988add91baf
Sarah Kuny <sarahkuny@icloud.com>
2022-06-13 19:29:59 -0500

    complete POST users endpoint

--- routes/users.js
+++ routes/users.js
@@ -14,10 +14,10 @@ router.get('/', async function(req, res, next) {
     if (data.length){
         res.status(200).send(data)
     } else {
-        res.status(400).send("No users in database")
+        res.status(404).send("No users in database")
     }
    } catch (err) {
-        res.status(400).send(err)
+        res.status(500).send(err)
    }
 });
 
@@ -27,12 +27,29 @@ router.get('/:id', async function (req, res, next){
         const { data } = await db(`SELECT * FROM users WHERE id=${req.params.id};`);
         res.status(200).send(data)
     } catch (err){
-        res.status(400).send(err)
+        res.status(500).send(err)
     }
 })
 
 //POST user (require first_name, last_name, username, password --> store password as hashed password using bcrypt)
-router.post("/", function (req, res, next) {});
+router.post("/", async function (req, res, next) {
+    const { username, password, first_name, last_name } = req.body;
+    try{
+        //check if username taken
+        let { data } = await db(`SELECT username FROM users;`);
+        let userExists = data.find((user) => (user.username === username));
+        if (userExists) res.sendStatus(401);
+        //if username available, create hashed password and store in users table
+        if (userExists === undefined) {
+            const hashedPassword = await bcrypt.hash(password, 10);
+            await db(`INSERT INTO users (username, password, first_name, last_name) VALUES ("${username}", "${hashedPassword}", "${first_name}", "${last_name}");`)
+            res.status(200).send("user added");
+        }
+    } catch (err) {
+        res.status(500).send(err)
+    }
+    
+});
 
 //PUT: edit student
 router.put("/:id", function (req, res, next) {});
