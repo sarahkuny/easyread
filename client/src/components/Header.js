75acd0fa48ae12d8bda2f74577bc477cdc540abd
Sarah Kuny <sarahkuny@icloud.com>
2022-06-16 15:56:12 -0500

    change font on logo

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -5,7 +5,7 @@ export default function Header(props) {
     return (
         <>
             <div className="flex justify-between items-center h-24">
-                <h1 className="text-5xl ml-3">easyRead</h1>
+                <h1 className="text-5xl ml-3 font-louisgeorge">easy<b>Read</b></h1>
                 <div className="mr-3">
                     <button className="font-medium text-lg bg-black text-white m-1 py-2 px-5 rounded-md shadow-md hover:bg-sky-500">{props.buttonOne}</button>
                     <button className="font-medium text-lg bg-black text-white m-1 py-2 px-5 rounded-md shadow-md hover:bg-sky-500">{props.buttonTwo}</button>
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        louisgeorge: ["Louis George Cafe", "sans-serif"]
+      }
+    },
   },
   plugins: [],
 }
