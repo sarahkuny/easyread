81723d06ce128b8ad02c7926ab240bc8bb11b553
Sarah Kuny <sarahkuny@icloud.com>
2022-06-16 15:47:43 -0500

    change button hover color

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -7,8 +7,8 @@ export default function Header(props) {
             <div className="flex justify-between items-center h-24">
                 <h1 className="text-5xl ml-3">easyRead</h1>
                 <div className="mr-3">
-                    <button className="font-medium text-lg bg-black text-white m-1 py-2 px-5 rounded-md shadow-md hover:bg-blue-500">{props.buttonOne}</button>
-                    <button className="font-medium text-lg bg-black text-white m-1 py-2 px-5 rounded-md shadow-md hover:bg-blue-500">{props.buttonTwo}</button>
+                    <button className="font-medium text-lg bg-black text-white m-1 py-2 px-5 rounded-md shadow-md hover:bg-sky-500">{props.buttonOne}</button>
+                    <button className="font-medium text-lg bg-black text-white m-1 py-2 px-5 rounded-md shadow-md hover:bg-sky-500">{props.buttonTwo}</button>
                 </div>
             </div>
         </>
