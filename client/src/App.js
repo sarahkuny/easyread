bd13f270c3c66ae15bb7fb35c61e11c513a6a6e7
Sarah Kuny <sarahkuny@sarahs-air.lan>
2022-06-16 10:51:01 -0500

    config tailwind

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,12 @@
 import "./App.css";
+import "./output.css";
 
 function App() {
-  return <div className="App">HELLO FROM App.js</div>;
+  return (<>
+    <div className="App">HELLO FROM App.js</div>
+    <header className="border-gray-300 text-indigo-900">testing</header>
+  </>
+  )
 }
 
 export default App;
