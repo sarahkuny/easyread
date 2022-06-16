1634c8f9b83b435530e669327bf5357a82da4f87
Sarah Kuny <sarahkuny@icloud.com>
2022-06-16 16:56:00 -0500

    add skeleton for components

--- client/src/components/Banner.js
+++ client/src/components/Banner.js
@@ -1,2 +1,13 @@
 //Hero Banner - Reading Made Easy
 //Learn More, start reading buttons
+
+import React, {useState} from 'react';
+import { Link } from 'react-router-dom'
+
+export default function Banner(){
+
+    return(
+        <>
+        </>
+    )
+}
