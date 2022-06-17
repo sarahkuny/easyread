c63bbd8ddfbbd44d0aa17363b709e8a9210ed68c
Sarah Kuny <sarahkuny@sarahs-air.lan>
2022-06-17 18:20:01 -0500

    add upload form to converter

--- client/src/components/Converter.js
+++ client/src/components/Converter.js
@@ -4,14 +4,25 @@
 //Converted Text
 //Save Document Form
 //Help button with popup
-
-import React, {useState} from 'react';
+import axios from 'axios';
+import React, {useState, useEffect } from 'react';
 import { Link } from 'react-router-dom'
+import parse from 'html-react-parser';
+import Header from './Header';
+
 
 export default function Converter(){
 
     return(
         <>
+            <Header buttonOne="My Documents" buttonTwo="Sign Out" linkOne="/documents" linkTwo="/" />
+            <div className="w-5/6 h-screen bg-slate-50 m-auto">
+                <form className="bg-slate-100 w-full flex justify-evenly py-2 border">
+                    <label className="w-1/6 py-2">Attach Document</label>
+                    <input className="w-3/6"></input>
+                    <button className="bg-black rounded-md text-white px-4 py-2">Convert</button>
+                </form>
+            </div>
         </>
     )
 }
