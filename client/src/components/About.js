ca1d8c8b9710bfd5b3c6348f710b43e688eda741
ShannonGW <ShannonWilliams93@gmail.com>
2022-06-19 15:02:01 +0200

    format text container about page

--- client/src/components/About.js
+++ client/src/components/About.js
@@ -5,47 +5,53 @@
 //link to about bionic reading
 //example image of converted text
 import React, { useState } from "react";
-import { Link } from "react-router-dom";
+import Header from "./Header";
 
 export default function About() {
   return (
     <>
-      <Link to="/">
-        <h1 className="text-5xl ml-3 my-6 font-louisgeorge">
-          easy<b>Read</b>
-        </h1>
-      </Link>
-      <div className="bg-zinc-900">
-        <div className="flex flex-col md:flex-row m-auto lg:w-5/6 h-screen bg-zinc-900">
-          <div className="w-100 m-auto flex flex-col text-white">
-            <p className="text-center text-4xl">
-              What is easy<b>Read</b>?{" "}
-            </p>
-            {/* can't figure out how to center it and set max width so that it doesn't
-      expanded into on line across entire */}
-            <div className="text-center pt-2 text-2xl">
-              <p>
-                Reading is an enjoyable pastime for many, but for some, it can
-                be an unpleasant or even frustrating experience. Our goal with
-                this platform is to help aid those with learning difficulties,
-                such as Dyslexia, to read with greater ease.
-              </p>
-              <div>
-                <h2 className="mt-20 font-semibold">
-                  Example of Converted Text
-                </h2>
-                {/* <img src="converted-text-example.png" alt="converted text" /> */}
-                <h2 className="mt-20 font-semibold">Resources</h2>
-                <ul class="list-none">
-                  {/* <li>
+      <Header
+        buttonOne="Converter"
+        buttonTwo="Sign Out"
+        linkOne="/converter"
+        linkTwo="/"
+      />
+      <div className="flex flex-auto bg-zinc-900 h-screen mx-64">
+        <div className=" bg-zinc-900 flex flex-col text-white text-center mx-64">
+          <h2 className="w-50 text-center pt-2 text-2xl font-bold">
+            Who are we?
+          </h2>
+          <p className="text-center pt-2 text-xl">
+            Reading is an enjoyable pastime for many, but for some, it can be an
+            unpleasant or even frustrating experience. Our goal with easy
+            <b>Read</b> is to help aid those with learning difficulties, as well
+            as teachers, students, anyone who may benefit reading in a
+            non-traditional way.
+          </p>
+
+          <h2 className="w-50 text-center pt-2 text-2xl font-bold">
+            What is Bionic Reading?
+          </h2>
+          <p className="text-center pt-2 text-xl">
+            easy<b>Read</b> helps improve its readers' comprehension and focus
+            by using the Bionic Reading method, which creates artificial
+            fixation points within the coverted text. Initial letters of each
+            word are bolded, so the reader is only focused on the highlighted
+            letters. The reader's brain is able to center complete each word,
+            creating a more shallow form of reading. As a result, readers are
+            able to experience a deeper understanding of the text.
+          </p>
+          {/* <img src="converted-text-example.png" alt="converted text" /> */}
+          <h2 className="w-50 text-center pt-2 text-2xl font-bold">
+            Resources
+          </h2>
+          <ul class="list-none">
+            {/* <li>
                     <link href="https://bionic-reading.com/about/">
                       About Bionic Reading
                     </link>
                   </li> */}
-                </ul>
-              </div>
-            </div>
-          </div>
+          </ul>
         </div>
       </div>
     </>
