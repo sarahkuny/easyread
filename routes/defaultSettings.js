bf43187ef09a5f1521ce05dd6ed56bc353a84090
Sarah Kuny <sarahkuny@icloud.com>
2022-06-18 19:28:06 -0500

    defaultSettings PUT endpoint works

--- routes/defaultSettings.js
+++ routes/defaultSettings.js
@@ -5,9 +5,7 @@ const userShouldBeLoggedIn = require("../guards/userShouldBeLoggedIn");
 
 router.use(express.json());
 
-// router.get("/", userShouldBeLoggedIn, function (req, res, next) {
-//   res.send("works")
-// });
+
 //GET settings by user_id
 router.get("/:user_id", userShouldBeLoggedIn, async function (req, res, next) {
   try{
@@ -24,13 +22,9 @@ router.get("/:user_id", userShouldBeLoggedIn, async function (req, res, next) {
 });
 
 //POST settings
-//(require user_id (foreign key from users table),font_size, font_color, background_color, line_spacing)
-// Do I put the inner join here? or is it in users table
-// "id": 1, "user_id": 1, "font_size":14, "font_color": "black", "background_color": "purple", "line_spacing": 2
 router.post("/", userShouldBeLoggedIn, async function (req, res, next) {
   const { font_size, font_color, background_color, line_spacing, fixation, saccade } = req.body;
   const username = req.username;
-  console.log(username);
 
   try {
     const results = await db(`SELECT id FROM users WHERE username="${username}";`);
@@ -48,25 +42,20 @@ router.post("/", userShouldBeLoggedIn, async function (req, res, next) {
 //PUT: edit settings
 //user_id, font_size, font_color, background_color, line_spacing
 // https://faridho.gitbooks.io/create-rest-api-by-node-js-express-js-and-mysql/content/put-method.html
-router.put("/:id", userShouldBeLoggedIn, function (req, res, next) {
-  const sql =
-    'UPDATE default_settings SET user_id = ?, font_size = ?, font_color = ?, background_color = ? line_spacing = ? WHERE id="' +
-    req.body.id +
-    '"';
-  const body = [
-    req.body.user_id,
-    req.body.font_size,
-    req.body.font_color,
-    req.body.background_color,
-    req.body.line_spacing,
-  ];
-  con.query(sql, body, function (err) {
-    if (err) {
-      res.json({ Error: true, Message: "Error execute sql" });
-    } else {
-      res.json({ Error: false, Message: "Success" });
-    }
-  });
+router.put("/:user_id", userShouldBeLoggedIn, async function (req, res, next) {
+  const { font_size, font_color, background_color, line_spacing, fixation, saccade } = req.body;
+  const username = req.username;
+
+  try{
+    const results = await db(`SELECT id FROM users WHERE username="${username}";`);
+    const user = results.data[0];
+    await db(`UPDATE default_settings SET font_size=${font_size}, font_color="${font_color}", background_color="${background_color}", line_spacing=${line_spacing}, fixation=${fixation}, saccade=${saccade} WHERE user_id=${user.id}`);
+    const { data } = await db(`SELECT * FROM default_settings;`);
+    res.status(200).send(data)
+  } catch (err) {
+    res.status(400).send(err)
+  }
+  
 });
 
 //DELETE settings (set back to app default)
