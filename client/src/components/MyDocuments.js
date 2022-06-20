36c33da18a51fdc423f79c778a3314b36b4b2328
ShannonGW <ShannonWilliams93@gmail.com>
2022-06-20 11:32:37 +0200

    add header to MyDocuments.js

--- client/src/components/MyDocuments.js
+++ client/src/components/MyDocuments.js
@@ -1,14 +1,20 @@
 //Header - go to converter, sign out
 //Documents
-    //each row: document title, share (email popup), delete
+//each row: document title, share (email popup), delete
 
-import React, {useState} from 'react';
-import { Link } from 'react-router-dom'
+import React, { useState } from "react";
+import Header from "./Header";
+import { Link } from "react-router-dom";
 
-export default function MyDocuments(){
-
-    return(
-        <>
-        </>
-    )
+export default function MyDocuments() {
+  return (
+    <>
+      <Header
+        buttonOne="Converter"
+        buttonTwo="Sign Out"
+        linkOne="/converter"
+        linkTwo="/"
+      />
+    </>
+  );
 }
