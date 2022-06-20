3611fe1c69df55ab1c8f9c61e5be370f98b026a8
ShannonGW <ShannonWilliams93@gmail.com>
2022-06-20 13:44:47 +0200

    style container body of  MyDocuments.js

--- client/src/components/MyDocuments.js
+++ client/src/components/MyDocuments.js
@@ -15,21 +15,31 @@ export default function MyDocuments() {
         linkOne="/converter"
         linkTwo="/"
       />
-      <div>
-        <div>
-          <div>
-            <h2>Document Name</h2>
-            <div className="border">
-              <ol>Document Title</ol>
-              <li>
-                EXAMPLE DOCUMENT
-                <button className="border">Share</button>
-                <button className="border">Delete</button>
-              </li>
-            </div>
+      <body className="w-auto	 bg-slate-50 m-auto h-screen shadow-2xl flex justify-center items-center">
+        <div className="flex-col h-full m-auto shadow-2xl w-5/6	">
+          <div className="bg-slate-900 text-white font-bold flex justify-start p-4 text-lg">
+            Document Name
+          </div>
+          <div>Document Title</div>
+          <div className="flex items-center justify-center">
+            <ol>
+              <li>DOC 1</li>
+              <li>DOC 1</li>
+              <li>DOC 3</li>
+            </ol>
           </div>
         </div>
-      </div>
+
+        {/* <button className="items-center py-2 px-2 text-black  rounded-lg my-6 w-60 hover:bg-sky-300 text-2xl">
+          <a
+            href="https://bionic-reading.com/about/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Learn More
+          </a>
+        </button> */}
+      </body>
     </>
   );
 }
