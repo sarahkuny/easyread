dfdad07ce84c2625d8e547b832aa3f74653b165b
ShannonGW <ShannonWilliams93@gmail.com>
2022-06-21 13:39:50 +0200

    add section to about page and fix header size

--- client/src/components/MyDocuments.js
+++ client/src/components/MyDocuments.js
@@ -2,11 +2,37 @@
 //Documents
 //each row: document title, share (email popup), delete
 
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Header from "./Header";
 import { Link } from "react-router-dom";
 
 export default function MyDocuments() {
+  const [documents, setDocuments] = useState([]);
+
+  useEffect(() => {
+    getAllDocuments();
+  }, []);
+
+  const getAllDocuments = () => {
+    fetch("/api/media")
+      //GET all 'users' from users table from db() declared in users.js
+      .then((response) => response.json())
+      .then((allDocuments) => {
+        setDocuments(documents); //add all users to the setUsers state so they can render in the dropdown
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  const handleShare = (event) => {
+    console.log("share button clicked!!");
+  };
+
+  const handleDelete = (event) => {
+    console.log("delete button clicked!!");
+  };
+
   return (
     <>
       <Header
@@ -22,19 +48,46 @@ export default function MyDocuments() {
           </div>
           <div className="m-3 p-3 border flex flex-col">
             <h1 className="text-xl ">Document Title</h1>
-            <div className=" text-l flex w-full list-decimal   w-full flex justify-between">
-                <p className="px-4 py-2 flex  items-center">
-                  DOC 1 </p>
-                  <div>
-                  <button className=" rounded-lg hover:bg-sky-300 bg-black text-white text-l py-1 px-2 m-2">
-                    Share
-                  </button>
-                  <button className="rounded-lg items-center hover:bg-sky-300  bg-black text-white text-l py-1 px-2 m-2">
-                    Delete
-                  </button>
-                  </div>
-                
+            <div className=" text-l flex w-full list-decimal  justify-between">
+              <p className="px-4 py-2 flex  items-center">
+                {documents.map((document) => {
+                  document.name;
+                })}
+              </p>
+
+              <div>
+                <button
+                  onClick={handleShare}
+                  className=" rounded-lg hover:bg-sky-300 bg-black text-white text-l py-1 px-2 m-2"
+                >
+                  Share
+                </button>
+                <button
+                  onClick={handleDelete}
+                  className="rounded-lg items-center hover:bg-sky-300  bg-black text-white text-l py-1 px-2 m-2"
+                >
+                  Delete
+                </button>
+              </div>
             </div>
+            {/* <div className=" text-l flex w-full list-decimal  justify-between">
+              <p className="px-4 py-2 flex  items-center">DOC 1 </p>
+
+              <div>
+                <button
+                  onClick={handleShare}
+                  className=" rounded-lg hover:bg-sky-300 bg-black text-white text-l py-1 px-2 m-2"
+                >
+                  Share
+                </button>
+                <button
+                  onClick={handleDelete}
+                  className="rounded-lg items-center hover:bg-sky-300  bg-black text-white text-l py-1 px-2 m-2"
+                >
+                  Delete
+                </button>
+              </div>
+            </div> */}
           </div>
         </div>
       </body>
