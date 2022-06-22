376c8cb06c8b7849f9596f808d44f6a6c5b52811
ShannonGW <ShannonWilliams93@gmail.com>
2022-06-22 18:12:50 +0200

    restyle documents page and fetch documents from backend

--- client/src/components/MyDocuments.js
+++ client/src/components/MyDocuments.js
@@ -71,7 +71,7 @@ export default function MyDocuments() {
               Saved Documents
             </h1>
           </div>
-          <div className="flex-col h-screen l m-auto w-5/6 shadow-2xl bg-white rounded-t-xl ">
+          <div className="flex-col h-screen l m-auto w-5/6 shadow-2xl bg-white rounded-t-xl overflow-y-auto max-height-48 ">
             <div className="bg-slate-900 rounded-t-xl">
               <table className="w-full text-white overflow-y-auto  max-height-80">
                 <thead>
@@ -81,86 +81,6 @@ export default function MyDocuments() {
                   </tr>
                 </thead>
                 <tbody>
-                  {/* ROW 1  correct format*/}
-                  <tr className="odd:bg-white even:bg-sky-100 text-black">
-                    <td className="p-3 px-3 text-left">DOCUMENT TEST 1</td>
-                    <td className="p-3 text-right pr-14">
-                      <button
-                        onClick={handleShare}
-                        className="rounded-lg hover:bg-sky-300 bg-black text-white text-l py-1 px-2 m-2"
-                      >
-                        <svg
-                          xmlns="http://www.w3.org/2000/svg"
-                          className="h-6 w-6"
-                          viewBox="0 0 20 20"
-                          fill="currentColor"
-                        >
-                          <path d="M15 8a3 3 0 10-2.977-2.63l-4.94 2.47a3 3 0 100 4.319l4.94 2.47a3 3 0 10.895-1.789l-4.94-2.47a3.027 3.027 0 000-.74l4.94-2.47C13.456 7.68 14.19 8 15 8z" />
-                        </svg>
-                      </button>
-                      <button
-                        onClick={handleDelete}
-                        className="rounded-lg hover:bg-sky-300 bg-black text-white text-l py-1 px-2 m-2"
-                      >
-                        <svg
-                          xmlns="http://www.w3.org/2000/svg"
-                          className="h-6 w-6"
-                          fill="none"
-                          viewBox="0 0 24 24"
-                          stroke="currentColor"
-                          stroke-width="2"
-                        >
-                          <path
-                            stroke-linecap="round"
-                            stroke-linejoin="round"
-                            d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16"
-                          />
-                        </svg>
-                      </button>
-                    </td>
-                  </tr>
-                  {/* row 2 */}
-                  <tr className="odd:bg-white even:bg-sky-100 text-black">
-                    <td className="p-3 px-3 text-left">DOCUMENT TEST 2</td>
-                    <td className="p-3 text-right pr-14">
-                      <button
-                        onClick={handleShare}
-                        className="rounded-lg hover:bg-sky-300 bg-black text-white text-l py-1 px-2 m-2"
-                      >
-                        <svg
-                          xmlns="http://www.w3.org/2000/svg"
-                          className="h-6 w-6"
-                          viewBox="0 0 20 20"
-                          fill="currentColor"
-                        >
-                          <path d="M15 8a3 3 0 10-2.977-2.63l-4.94 2.47a3 3 0 100 4.319l4.94 2.47a3 3 0 10.895-1.789l-4.94-2.47a3.027 3.027 0 000-.74l4.94-2.47C13.456 7.68 14.19 8 15 8z" />
-                        </svg>
-                      </button>
-                      <button
-                        onClick={handleDelete}
-                        className="rounded-lg hover:bg-sky-300 bg-black text-white text-l py-1 px-2 m-2"
-                      >
-                        <svg
-                          xmlns="http://www.w3.org/2000/svg"
-                          className="h-6 w-6"
-                          fill="none"
-                          viewBox="0 0 24 24"
-                          stroke="currentColor"
-                          stroke-width="2"
-                        >
-                          <path
-                            stroke-linecap="round"
-                            stroke-linejoin="round"
-                            d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16"
-                          />
-                        </svg>
-                      </button>
-                    </td>
-                  </tr>
-                  {/* end of row 2 */}
-
-                  {/* row 3 */}
-
                   {documents.map((document) => {
                     return (
                       <tr className="odd:bg-white even:bg-sky-100 text-black">
@@ -203,41 +123,6 @@ export default function MyDocuments() {
                       </tr>
                     );
                   })}
-
-                  {/* end of row 3 */}
-
-                  {/* row 4 -NOT CORRECT */}
-                  <tr className="odd:bg-white even:bg-sky-100 text-black">
-                    {documents.map((document) => {
-                      return (
-                        <div>
-                          <td>{document.name}</td>
-                          <button>Share</button>
-                          <button
-                            onClick={handleDelete}
-                            className="rounded-lg items-center hover:bg-sky-300  bg-black text-white text-l py-1 px-2 m-2"
-                          >
-                            Delete
-                          </button>
-                          <td>
-                            <button
-                              onClick={handleShare}
-                              className="rounded-lg hover:bg-sky-300 bg-black text-white text-l py-1 px-2 m-2"
-                            >
-                              Share
-                            </button>
-                            <button
-                              onClick={handleDelete}
-                              className="rounded-lg items-center hover:bg-sky-300  bg-black text-white text-l py-1 px-2 m-2"
-                            >
-                              Delete
-                            </button>
-                          </td>
-                        </div>
-                      );
-                    })}
-                  </tr>
-                  {/* end of row 4 */}
                 </tbody>
               </table>
             </div>
@@ -246,40 +131,4 @@ export default function MyDocuments() {
       </div>
     </>
   );
-}
-
-{
-  /* <tbody>
-  <tr className="odd:bg-white even:bg-sky-100 text-black">
-    <td className="p-3 px-3 text-left">DOCUMENT TEST</td>
-    <td className="p-3 text-right pr-14">
-      <button className="rounded-lg hover:bg-sky-300 bg-black text-white text-l py-1 px-2 m-2">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          class="h-6 w-6"
-          viewBox="0 0 20 20"
-          fill="currentColor"
-        >
-          <path d="M15 8a3 3 0 10-2.977-2.63l-4.94 2.47a3 3 0 100 4.319l4.94 2.47a3 3 0 10.895-1.789l-4.94-2.47a3.027 3.027 0 000-.74l4.94-2.47C13.456 7.68 14.19 8 15 8z" />
-        </svg>
-      </button>
-      <button className="rounded-lg hover:bg-sky-300 bg-black text-white text-l py-1 px-2 m-2">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          class="h-6 w-6"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke="currentColor"
-          stroke-width="2"
-        >
-          <path
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16"
-          />
-        </svg>
-      </button>
-    </td>
-  </tr>
-</tbody>; */
 }
