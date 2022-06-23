0e39372d1edc208bfc41e1339fea7d1c25aedcf5
Sarah Kuny <sarahkuny@sarahs-air.lan>
2022-06-23 00:26:23 -0500

    send data on delete route

--- routes/media.js
+++ routes/media.js
@@ -72,7 +72,7 @@ router.delete("/:id", userShouldBeLoggedIn, async function (req, res, next) {
     await db(`DELETE FROM shared WHERE media_id=${req.params.id};`);
     await db(`DELETE FROM media WHERE id=${req.params.id};`);
     const { data } = await db(`SELECT * FROM media;`);
-    res.status(200).send("media deleted");
+    res.status(200).send(data);
   } catch (err) {
     res.status(500).send(err);
   }
