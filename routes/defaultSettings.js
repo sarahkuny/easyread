8dbac92f9e8e58eaa891c17dc6152a0f26258468
Sarah Kuny <sarahkuny@icloud.com>
2022-06-28 11:00:09 -0500

    fix default settings put

--- routes/defaultSettings.js
+++ routes/defaultSettings.js
@@ -39,7 +39,7 @@ router.post("/", userShouldBeLoggedIn, async function (req, res, next) {
     await db(
       `INSERT INTO default_settings (user_id, font_size, font_color, background_color, line_spacing) VALUES (${user.id}, ${font_size}, "${font_color}", "${background_color}", ${line_spacing});`
     );
-    const { data } = await db(`SELECT * FROM default_settings;`);
+    const { data } = await db(`SELECT * FROM default_settings where user_id = ${user.id};`);
     res.status(200).send(data);
   } catch (err) {
     res.status(400).send(err);
@@ -51,12 +51,10 @@ router.post("/", userShouldBeLoggedIn, async function (req, res, next) {
 // https://faridho.gitbooks.io/create-rest-api-by-node-js-express-js-and-mysql/content/put-method.html
 router.put("/:user_id", userShouldBeLoggedIn, async function (req, res, next) {
   const {
-    fontSize,
-    fontColor,
-    backgroundColor,
-    lineSpacing,
-    fixation,
-    saccade,
+    font_size,
+    font_color,
+    background_color,
+    line_spacing,
   } = req.body;
   const username = req.username;
 
@@ -66,20 +64,20 @@ router.put("/:user_id", userShouldBeLoggedIn, async function (req, res, next) {
     );
     const user = results.data[0];
     await db(
-      `UPDATE default_settings SET font_size=${font_size}, font_color="${font_color}", background_color="${background_color}", line_spacing=${line_spacing}, fixation=${fixation}, saccade=${saccade} WHERE user_id=${user.id}`
+      `UPDATE default_settings SET font_size=${font_size}, font_color="${font_color}", background_color="${background_color}", line_spacing=${line_spacing} WHERE user_id=${user.id}`
     );
-    const { data } = await db(`SELECT * FROM default_settings;`);
+    const { data } = await db(`SELECT * FROM default_settings where user_id = ${user.id};`);
     res.status(200).send(data);
   } catch (err) {
     res.status(400).send(err);
   }
 });
 
-//DELETE settings (set back to app default)
-router.delete("/:id", userShouldBeLoggedIn, function (req, res, next) {
-  db(`DELETE FROM default_settings WHERE id=${req.params.id};`)
-    .then((results) => res.send(results))
-    .catch((err) => res.status(500).send(err));
-});
+//Delete if user deletes account
+// router.delete("/:id", userShouldBeLoggedIn, function (req, res, next) {
+//   db(`DELETE FROM default_settings WHERE id=${req.params.id};`)
+//     .then((results) => res.send(results))
+//     .catch((err) => res.status(500).send(err));
+// });
 
 module.exports = router;
