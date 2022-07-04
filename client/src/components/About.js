b25ce819bfc0a4542c514d2d95a161c6d8be103d
Sarah Kuny <sarahkuny@icloud.com>
2022-07-04 10:16:57 -0500

    about page mobile responsive

--- client/src/components/About.js
+++ client/src/components/About.js
@@ -10,13 +10,13 @@ export default function About() {
       <Header />
       {/* Banner */}
       <div className="snap-y">
-        <div className=" bg-zinc-900 h-min-content flex flex-col items-center">
-                <div className="bg-aboutBanner bg-cover h-[966px] w-[1440px] flex flex-col justify-center">
+        <div className=" bg-zinc-900 flex flex-col items-center">
+                <div className="bg-aboutBanner bg-cover bg-center flex flex-col justify-center items-center h-screen w-full   ">
                 <h2 className="text-white text-center text-7xl ">ABOUT</h2>
                 </div>
         </div>
         {/* Mission */}
-        <div className="snap-center bg-zinc-900 flex items-center justify-center">
+        <div className="snap-center bg-zinc-900 flex flex-col-reverse lg:flex-row items-center justify-center h-screen">
             <div>
                 <div className="w-[720px] flex justify-center">
                     <img src={sarah} alt="Sarah Kuny" className="rounded-full h-[200px] w-[200px] mr-10"/>
@@ -25,10 +25,11 @@ export default function About() {
                 <div className="flex justify-center">
                     <img src={hannah} alt="Hannah Willows" className="rounded-full h-[200px] w-[200px]"/>
                 </div>
+                <div className="text-white italic text-center pt-10 font-louisgeorge">Creators Sarah Kuny, Shannon Williams, and Hannah Willows</div>
             </div>
-            <div className="w-[720px] text-center pt-10 h-screen text-white flex flex-col items-center justify-center">
-                <h3 className="text-7xl font-louisgeorge mb-10">Our Mission</h3>
-                <p className="w-4/6 text-xl">
+            <div className="w-[720px] text-center pt-10  text-white flex flex-col items-center justify-center">
+                <h3 className="text-7xl font-louisgeorge ">Our Mission</h3>
+                <p className="w-4/6 text-xl pb-10">
                     easyRead originated as a final project for a coding bootcamp. The team behind the app 
                     consisted of two teachers, and an occupational therapist who worked with stroke victims. 
                     They all saw a need for a tool that could transform the reading process - making it accessible 
@@ -38,8 +39,8 @@ export default function About() {
         </div>
         
         {/* What is Bionic Reading */}
-        <div className="snap-center bg-zinc-900 flex items-center justify-center">
-            <div className="w-[720px] text-center pt-10 h-screen text-white flex flex-col items-center justify-center">
+        <div className="snap-center bg-zinc-900 flex flex-col-reverse lg:flex-row items-center justify-center h-screen">
+            <div className="lg:w-[720px] text-center pt-10  text-white flex flex-col items-center justify-center">
                 <h3 className="text-7xl font-louisgeorge mb-10">What is Bionic Reading?</h3>
                 <p className="w-4/6 text-xl">
                 One of the tools easyRead provides to help improve users' reading experience 
@@ -48,7 +49,7 @@ export default function About() {
                 This allows the reader to center and complete words, creating a shallow form of reading. 
                 As a result, readers have more bandwith to focus on text comprehension.</p>
             </div>
-            <div className="w-[720px] flex justify-center mr-20">
+            <div className="flex justify-center px-5 lg:w-[720px] lg:mr-20">
                 <img src={bionicExample} alt="bionic reading text example" className="rounded" />
             </div>
         </div>
